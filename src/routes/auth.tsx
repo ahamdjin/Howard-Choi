8d9d12210a4cedd54530f84ee0fc0335204605a3
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-10 06:09:43 +0500

    Mark auth route noindex

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Auth from "@/pages/Auth";
+import { noIndexSeo } from "@/lib/seo";
 
 export const Route = createFileRoute("/auth")({
+  head: () => noIndexSeo("Sign in | Howard Choi Law"),
   ssr: false,
   component: Auth,
 });
