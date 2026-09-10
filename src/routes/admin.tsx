062c2c8989981c0f7bcf40db2c87bddaf8ba984c
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-10 06:09:32 +0500

    Mark admin route noindex

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Admin from "@/pages/Admin";
+import { noIndexSeo } from "@/lib/seo";
 
 export const Route = createFileRoute("/admin")({
+  head: () => noIndexSeo("Admin | Howard Choi Law"),
   ssr: false,
   component: Admin,
 });
