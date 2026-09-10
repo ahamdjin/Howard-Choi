28b7779d587383a5ea906a6b1926c69ebe769e69
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-10 06:10:06 +0500

    Keep legacy location detail routes out of search results

--- src/routes/location.$id.tsx
+++ src/routes/location.$id.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import LocationDetail from "@/pages/LocationDetail";
+import { noIndexSeo } from "@/lib/seo";
 
 export const Route = createFileRoute("/location/$id")({
+  head: () => noIndexSeo("Location | Howard Choi Law"),
   component: LocationDetail,
 });
