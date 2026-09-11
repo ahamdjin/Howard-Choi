f57e54c053b00b2ef617538724370605353e9628
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-12 02:47:03 +0500

    Use redesigned attorney page

--- src/routes/attorney.tsx
+++ src/routes/attorney.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { AttorneyPage } from "@/pages/InjurySitePages";
+import { AttorneyPage } from "@/pages/EditorialInnerPages";
 import { buildSeo } from "@/lib/seo";
 
 export const Route = createFileRoute("/attorney")({
