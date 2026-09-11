a36ab2ce80051f128c552d713e4de4e115a37bc5
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-12 02:47:16 +0500

    Use redesigned practice areas page

--- src/routes/practice-areas.tsx
+++ src/routes/practice-areas.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { PracticeAreasPage } from "@/pages/InjurySitePages";
+import { PracticeAreasPage } from "@/pages/EditorialInnerPages";
 import { buildSeo } from "@/lib/seo";
 
 export const Route = createFileRoute("/practice-areas")({
