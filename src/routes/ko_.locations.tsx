c8ad7a42d86ad633d175acb35ccc9e7d856801b9
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-12 22:16:23 +0500

    Use refined Korean locations design

--- src/routes/ko_.locations.tsx
+++ src/routes/ko_.locations.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { LocationsPage } from "@/pages/EditorialLocationPages";
+import { LocationsPage } from "@/pages/RefinedLocationPages";
 import { buildSeo } from "@/lib/seo";
 
 export const Route = createFileRoute("/ko/locations")({
