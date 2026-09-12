8889016fb53f4196ae490ec5d2f4a2448a96e2d3
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-12 22:16:10 +0500

    Use refined Korean practice detail design

--- src/routes/ko_.practice-areas_.$slug.tsx
+++ src/routes/ko_.practice-areas_.$slug.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute, notFound } from "@tanstack/react-router";
-import { PracticeAreaDetailPage } from "@/pages/EditorialInnerPages";
+import { PracticeAreaDetailPage } from "@/pages/RefinedInnerPages";
 import { getPracticeArea } from "@/data/injurySite";
 import { breadcrumbJsonLd, buildSeo } from "@/lib/seo";
 
