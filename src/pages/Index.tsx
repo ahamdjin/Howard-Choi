af08c62ca7f1c5a66463237d139f7f68c2478600
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-16 03:58:02 +0500

    design: switch homepage to simplified DK-inspired section system

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,34 +1,16 @@
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
-import FirmSnapshot from "@/components/FirmSnapshot";
-import Locations from "@/components/Locations";
-import Experience from "@/components/Experience";
-import SelectedResult from "@/components/SelectedResult";
-import DirectAccess from "@/components/DirectAccess";
-import ClientSuccessFeature from "@/components/ClientSuccessFeature";
-import FAQ from "@/components/FAQ";
-import Booking from "@/components/Booking";
+import HomeSections from "@/components/HomeSections";
 import Footer from "@/components/Footer";
 
 const Index = () => (
-  <div className="home-page min-h-[100svh] overflow-x-clip bg-background">
+  <div className="home-page min-h-[100svh] overflow-x-clip bg-[#f7f6f2]">
     <Navigation />
-    <div className="relative">
-      <div className="sticky top-0 z-0 h-[100svh] w-full"><Hero /></div>
-      <div className="relative z-10 w-full bg-background">
-        <FirmSnapshot />
-        <Locations />
-      </div>
-    </div>
-    <div className="relative z-20 bg-background">
-      <Experience />
-      <SelectedResult />
-      <DirectAccess />
-      <ClientSuccessFeature />
-      <FAQ />
-      <Booking />
-      <Footer />
-    </div>
+    <main>
+      <Hero />
+      <HomeSections />
+    </main>
+    <Footer />
   </div>
 );
 
