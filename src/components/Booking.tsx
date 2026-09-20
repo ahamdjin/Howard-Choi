fc2f439d722834b6dc6f7d18fab15383f36e6ae7
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-20 18:39:46 +0500

    Keep Suite 216 consistent in business information

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -46,12 +46,12 @@ const Booking = () => {
             </a>
           </div>
           <a
-            href="https://www.google.com/maps/search/?api=1&query=6301+Beach+Blvd%2C+Buena+Park%2C+CA+90621"
+            href="https://www.google.com/maps/search/?api=1&query=6301+Beach+Blvd%2C+Suite+216%2C+Buena+Park%2C+CA+90621"
             target="_blank"
             rel="noreferrer"
             className="mt-6 inline-block text-[12px] text-white/42 transition-colors hover:text-white/68"
           >
-            6301 Beach Blvd, Buena Park, CA 90621
+            6301 Beach Blvd, Suite 216, Buena Park, CA 90621
           </a>
         </motion.div>
       </div>
