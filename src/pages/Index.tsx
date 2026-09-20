0072e61146f4c3ad8a2e2ca647352509e1787a42
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-20 18:05:43 +0500

    Use existing attorney section instead of duplicate trust preview

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,7 +8,6 @@ import DirectAccess from "@/components/DirectAccess";
 import ClientSuccessFeature from "@/components/ClientSuccessFeature";
 import FAQ from "@/components/FAQ";
 import Booking from "@/components/Booking";
-import HomeAttorneyTrustPreview from "@/components/HomeAttorneyTrustPreview";
 import Footer from "@/components/Footer";
 
 const Index = () => (
@@ -28,7 +27,6 @@ const Index = () => (
       <ClientSuccessFeature />
       <FAQ />
       <Booking />
-      <HomeAttorneyTrustPreview />
       <Footer />
     </div>
   </div>
