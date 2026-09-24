7506076a9be5a2f05c5d8730adb554c69712044d
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-25 03:12:08 +0500

    Add Google Tag Manager

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -82,8 +82,24 @@ function RootDocument({ children }: { children: ReactNode }) {
 
   return (
     <html lang={lang}>
-      <head><HeadContent /></head>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtM.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5V5GSC7B');".replace("gtM.js", "gtm.js"),
+          }}
+        />
+        <HeadContent />
+      </head>
       <body className={bodyClassName}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5V5GSC7B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <div className="min-h-screen bg-background md:px-[7px] md:pb-[7px]">
           {children}
         </div>
