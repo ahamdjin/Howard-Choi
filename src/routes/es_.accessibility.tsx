0342719a830889ada8b91fd53a7582c38559dfb7
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-26 23:00:07 +0500

    Use exact legal-page structure for Spanish

--- src/routes/es_.accessibility.tsx
+++ src/routes/es_.accessibility.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { SpanishLegalPage } from "@/pages/es/EsLegalPages";
+import { LegalPage } from "@/pages/LegalPages";
 import { buildSeo } from "@/lib/seo";
 
 export const Route = createFileRoute("/es_/accessibility")({
@@ -11,5 +11,5 @@ export const Route = createFileRoute("/es_/accessibility")({
     noindex: true,
     followWhenNoindex: true,
   }),
-  component: () => <SpanishLegalPage type="accessibility" />,
+  component: () => <LegalPage type="accessibility" locale="es" />,
 });
